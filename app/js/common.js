06715f2ac3de2c270fbbcb7c2bb7a789fd7cd47f
Alexander Kuchuk <dafaqtx@gmail.com>
2018-08-05 16:45:58 +0600

    add reviews block and include slick slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
-	
+	$('.js-reviews-slider').slick({
+        infiniti: true,
+        speed: 700,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        dotsClass: 'reviews__dots flex flex__align-center',
+    });
 
 });
