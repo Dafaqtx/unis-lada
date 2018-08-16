27d8a1d3640d8f8661dd2c1077475b63c550384e
Alexander Kuchuk <dafaqtx@gmail.com>
2018-08-16 22:41:22 +0600

    add active statement for header elements

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,56 @@
 $(function() {
 
+	// open menu
+	$('body').on('click', '.header__sidebar-more', function() {
+		
+		if ( $('.header__sidebar-item').hasClass('header__sidebar-item--hidden') ) {
+			$('.header__sidebar-item').removeClass('header__sidebar-item--hidden')
+		} else {
+			$('.header__sidebar-item:gt(2)').addClass('header__sidebar-item--hidden');
+		}
+
+	});
+
+	// search btn
+	$('body').on('click', '.header__search-btn', function() {
+		
+		$('.header__operators').toggle();
+		$('.header__search').toggleClass('header__search--open flex flex__align-justify');
+
+		let searchInput = $('.header__search-input');
+
+		if ( searchInput.val() === '' ) {
+			return false;
+		}
+
+	})
+
+	var counter = 0;
+	// add address field
+	$('body').on('click', '#js-add-address', function() {
+		counter++;
+		console.log(counter);
+		$('.calc__form-input--add').append('<input type="text" name="from-'+ counter +'" class="calc__form-input calc__form-input--more" placeholder="(начните вводить адрес)">');
+	});
+
+	// add comment field
+	$('body').on('click', '#js-add-comment', function() {
+		$('.calc__form-add-textarea').toggle('calc__form-add-textarea--active');
+		$(this).parent('.calc__form-add-comment').toggleClass('calc__form-add-comment--open');
+
+		setTimeout(function() {
+			let blockVisibility = $('.calc__form-add-textarea').css('display');
+
+			if ( blockVisibility === 'block' ) {
+				$('#js-add-comment').text('Свернуть комментарий');
+			} else {
+				$('#js-add-comment').text('Добавить комментарий');
+			}
+
+		}, 500);
+	})
+
+	// init slider reviews
 	$('.js-reviews-slider').slick({
         infiniti: true,
         speed: 700,
@@ -10,6 +61,8 @@ $(function() {
         dotsClass: 'reviews__dots flex flex__align-center',
     });
 
+
+		//init slider partners
     $('.js-patrners-slider').slick({
         infiniti: true,
         speed: 700,
@@ -25,7 +78,8 @@ $(function() {
     $('body').on('click', '.calc__form-example', function(){
         let address = $(this).text();
 
-        $('.calc__form-input--from').val(address);
+				$('.calc__form-input--from').val(address);
+				$('.calc__form-input--from').focus();
     })
 
 
@@ -39,8 +93,13 @@ $(function() {
     // tabs
     $('.d-tabs__list').on('click', 'li:not(.d-tabs__content--active)', function() {
         $(this)
-          .addClass('d-tabs__item--active').siblings().removeClass('d-tabs__item--active')
-          .closest('.d-tabs').find('.d-tabs__content').removeClass('d-tabs__content--active').eq($(this).index()).addClass('d-tabs__content--active');
+					.addClass('d-tabs__item--active')
+					.siblings()
+					.removeClass('d-tabs__item--active')
+					.closest('.d-tabs')
+					.find('.d-tabs__content')
+					.removeClass('d-tabs__content--active')
+					.eq($(this).index()).addClass('d-tabs__content--active');
     });
 
     // accordion
@@ -56,9 +115,10 @@ $(function() {
         
         if( !$(this).find('.d-accordion__sub-list').is(":visible") ) {
 
-			$(this).find('.d-accordion__sub-list').addClass('d-accordion__sub-list--active');
-			$(this).find('.d-accordion__sub-list').slideDown();
-		}
+					$(this).find('.d-accordion__sub-list').addClass('d-accordion__sub-list--active');
+					$(this).find('.d-accordion__sub-list').slideDown();
+
+				}
 
     });
 
